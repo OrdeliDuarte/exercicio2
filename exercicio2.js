46736b68060130e17b8dabf25b2f66f2018dad9f
OrdeliDuarte <ordeliduarte25@gmail.com>
2024-08-29 16:39:49 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+// 1-Crie uma variável que receba e armazene a sua idade no tipo number
+let idade = 15
+// 2-Crie uma outra variável que receba o valor da sua primeira variável, mas com o seu tipo alterado para string                                                                                                                                                                                                                                                      bah
+let idadeAlterada = idade.toString()
+/* 3-Imprima duas mensagens no terminal:
+A primeira deve mostrar o valor e o tipo da primeira variável.
+A segunda deve mostrar o valor e o tipo da sugunda variável.*/
+console.log(idade, typeof idade)
+console.log(idadeAlterada, typeof idadeAlterada)
